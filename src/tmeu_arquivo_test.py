0ccffa04c5adbef6b107051b8337d93f8113faae
Rafael Ferreira <rarafa245@gmail.com>
2020-12-12 01:45:30 -0300

    first_test14

--- src/tmeu_arquivo_test.py
+++ src/tmeu_arquivo_test.py
@@ -1,3 +1,13 @@
 def test_something():
     v = ["cachorro", "gato"]
     assert "gato" in v
+
+
+def test_something2():
+    x = 3
+    isTrue = None
+
+    if x < 2:
+        isTrue = True
+
+    assert isTrue is not True
