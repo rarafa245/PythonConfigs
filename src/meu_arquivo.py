aa70b72ba8b138b2ba76b4879243392d4907351c
Rafael Ferreira <rarafa245@gmail.com>
2020-12-12 01:29:37 -0300

    first_test

---
+++ src/meu_arquivo.py
@@ -0,0 +1,20 @@
+""" Documento de teste para aula """
+
+
+def minha_funcao():
+    """ Apresentar uma mensagem """
+
+    print("Ola Mundo!")
+
+
+def somatorio(num1: int, num2: int) -> int:
+    """Retorna a soma entre dois numeros
+    :param  - num1: numero inteiro 1
+            - num2: numero inteiro 2
+    :return - Soma entre dois numeros"""
+
+    return num1 + num2
+
+
+minha_funcao()
+somatorio(2, 4)
